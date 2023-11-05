b2779d9dd5f1e1e5d6188cb6451b5e2d0241367f
Artem Melnyk <melkam464@gmail.com>
2023-11-05 22:42:48 +0100

    chore: add example

--- discrete-math/example.ts
+++ discrete-math/example.ts
@@ -1,6 +1,6 @@
 import { and, imp, not, or, v } from "./main.ts";
 
-// ((p → q) ∧ (r → q)) → ((¬q ∨ ¬q) → (¬p ∨ ¬r))
+// ((p→q)∧(r→q))→((¬q∨¬q)→(¬p∨¬r))
 const Q = imp(
   and(imp(v("p"), v("q")), imp(v("r"), v("q"))),
   imp(or(not(v("q")), not(v("q"))), or(not(v("p")), not(v("r")))),
@@ -19,3 +19,54 @@ try {
 // Conjuction ∧
 // Implication →
 // XOR ⊕
+
+try {
+  console.time("C");
+  console.log(
+    imp(
+      or(and(v("p"), v("q")), and(v("r"), v("s"))),
+      and(or(not(v("p")), not(v("q"))), or(not(v("r")), not(v("s")))),
+    ).can_be(1),
+  );
+} catch (error) {
+  console.error(error);
+} finally {
+  console.timeEnd("C");
+}
+
+try {
+  console.time("X");
+  // (p∧q∧r∧s∧t)→(¬u∨¬v∨¬w∨¬x∨¬y∨¬z)
+  and(
+    imp(v("p"), v("p")),
+    and(
+      imp(v("q"), v("q")),
+      and(
+        imp(v("r"), v("r")),
+        and(
+          imp(v("s"), v("s")),
+          and(
+            imp(v("t"), v("t")),
+            and(
+              imp(v("u"), v("u")),
+              and(
+                imp(v("v"), v("v")),
+                and(
+                  imp(v("w"), v("w")),
+                  and(
+                    imp(v("x"), v("x")),
+                    imp(v("y"), v("y")),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  ).can_be(1);
+} catch (error) {
+  console.error(error);
+} finally {
+  console.timeEnd("X");
+}
