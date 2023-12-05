dd79dd4995e0828b83e34b9cfe0132b02b601131
Artem Melnyk <melkam464@gmail.com>
2023-12-05 19:55:42 +0100

    java: done homework 8

--- java/homework8/src/main/java/homework8/AntSimulation.java
+++ java/homework8/src/main/java/homework8/AntSimulation.java
@@ -1,106 +1,103 @@
 package homework8;
 
 import java.util.Random;
+import java.util.concurrent.*;
 
 public class AntSimulation {
-  private static final char EMPTY = '.';
-  private static final char ANT = '8';
+    private static final char EMPTY = '.';
+    private static final char ANT = '8';
 
-  private char[][] grid;
-  private Thread[] ants;
+    private char[][] grid;
+    private Thread[] ants;
+    private ExecutorService executor;
+    private final int gridSize;
+    private final Random random;
 
-  private final int gridSize;
-  private final int antCount;
-  private final Random random;
+    public AntSimulation(int gridSize, int antCount) {
+        this.gridSize = gridSize;
+        this.random = new Random();
+        this.grid = new char[gridSize][gridSize];
+        this.ants = new Thread[antCount];
+        this.executor = Executors.newFixedThreadPool(antCount);
 
-  private HashMap<int[], Thread>
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                grid[i][j] = EMPTY;
+            }
+        }
 
-  public AntSimulation(int gridSize, int antCount) {
-    this.gridSize = gridSize;
-    this.antCount = antCount;
-    this.random = new Random();
-    this.grid = new char[gridSize][gridSize];
-    this.ants = new Thread[antCount];
+        for (int i = 0; i < antCount; i++) {
+            int randomX = random.nextInt(gridSize);
+            int randomY = random.nextInt(gridSize);
+            if (grid[randomX][randomY] == EMPTY) {
+                grid[randomX][randomY] = ANT;
+                ants[i] = createAnt(randomX, randomY);
+            }
 
-    for (int i = 0; i < gridSize; i++) {
-      for (int j = 0; j < gridSize; j++) {
-        grid[i][j] = EMPTY;
-      }
-    }
-    for (int i = 0; i < antCount; i++) {
-      int randomX = random.nextInt(gridSize);
-      int randomY = random.nextInt(gridSize);
-      grid[randomX][randomY] = ANT;
-      ants[i] = createAnt(randomX, randomY);
-    }
-  }
-
-  private Thread createAnt(int x, int y) {
-    return new Thread(() -> {
-      while (true) {
-        moveAnt(x, y);
-        sleepRandomTime();
-      }
-    });
-  }
-
-  static class NoAntToMoveException extends Exception {
-    public NoAntToMoveException(int x, int y) {
-      super(String.format("can't find ant on (%d, %d)", x, y));
-    }
-  }
-
-  static class MoveOutOfBoundException extends Exception {
-    public MoveOutOfBoundException(int x, int y) {
-      super(String.format("ant can't move to (%d, %d)", x, y));
-    }
-  }
-
-  private synchronized void moveAnt(int x, int y) {
-    int newX = x + random.nextInt(3) - 1;
-    int newY = y + random.nextInt(3) - 1;
-
-    if (newX < 0 || newX >= gridSize || newY < 0 || newY >= gridSize) {
-
+        }
     }
 
-    if (newX >= 0 && newX < gridSize && newY >= 0 && newY < gridSize) {
-      grid[x][y] = EMPTY;
-      x = newX;
-      y = newY;
-      grid[x][y] = ANT;
-      printGrid();
-    } else {
-      // Ant stays in place if the move is not valid
+    private Thread createAnt(int x, int y) {
+        return new Thread(() -> {
+            int antX = x;
+            int antY = y;
+            while (true) {
+                int[] newCoords = moveAnt(antX, antY);
+                antX = newCoords[0];
+                antY = newCoords[1];
+                try {
+                    Thread.sleep(random.nextInt(1000));
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
     }
 
-    if (x < 0 || x >= gridSize || y < 0 || y >= gridSize) {
-      System.out.println("Ant died! Out of grid!");
-    }
-  }
+    private synchronized int[] moveAnt(int oldX, int oldY) {
+        int newX = oldX + random.nextInt(3) - 1;
+        int newY = oldY + random.nextInt(3) - 1;
 
-  private void sleepRandomTime() {
-    try {
-      Thread.sleep(random.nextInt(1000));
-    } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
-    }
-  }
+        if (newX >= 0 && newX < gridSize && newY >= 0 && newY < gridSize) {
+            if (grid[newX][newY] == EMPTY) {
+                grid[oldX][oldY] = EMPTY;
+                grid[newX][newY] = ANT;
+                System.out.printf("Ant at (%d, %d) moves to (%d, %d):\n", oldX, oldY, newX, newY);
+                printGrid();
+                return new int[] { newX, newY };
+            }
+            return new int[] { oldX, oldY };
+        }
 
-  private void printGrid() {
-    System.out.println("Current state of the grid:");
-    for (int i = 0; i < gridSize; i++) {
-      for (int j = 0; j < gridSize; j++) {
-        System.out.print(grid[i][j] + " ");
-      }
-      System.out.println();
-    }
-    System.out.println();
-  }
+        System.out.printf("Ant at (%d, %d) dies new ant at (%d, %d):\n", oldX, oldY, newX, newY);
+        grid[oldX][oldY] = EMPTY;
 
-  public void startSimulation() {
-    for (Thread ant : ants) {
-      ant.start();
+        int randomX = random.nextInt(gridSize);
+        int randomY = random.nextInt(gridSize);
+
+        while (grid[randomX][randomY] == ANT) {
+            randomX = random.nextInt(gridSize);
+            randomY = random.nextInt(gridSize);
+        }
+        grid[randomX][randomY] = ANT;
+        printGrid();
+        return new int[] { randomX, randomY };
     }
-  }
+
+    private void printGrid() {
+        for (int x = 0; x < gridSize; x++) {
+            for (int y = 0; y < gridSize; y++) {
+                System.out.print(grid[x][y] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void start() {
+        for (Thread ant : ants) {
+            executor.execute(ant);
+        }
+        executor.shutdown();
+    }
 }
