e136355d4cd3c8f6d9b4684e96bc43d242ba83cf
Artem Melnyk <melkam464@gmail.com>
2025-02-20 14:10:32 +0100

    lab10 upd

--- concurrent-programming/src/main/java/lab4/Tury.java
+++ concurrent-programming/src/main/java/lab4/Tury.java
@@ -1,67 +1,71 @@
 package lab4;
 
 class Sync {
-  public int tura = 1;
+
+    public int tura = 1;
 }
 
 class T1 extends Thread {
-  private Sync sync;
 
-  public T1(Sync sync) {
-    this.sync = sync;
-  }
+    final private Sync sync;
 
-  @Override
-  public void run() {
-    for (int i = 0; i < 10; i++) {
-      synchronized (sync) {
-        while (sync.tura != 1) {
-          try {
-            sync.wait();
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
-        System.out.println(1);
-        sync.tura = 2;
-        sync.notifyAll();
-      }
+    public T1(Sync sync) {
+        this.sync = sync;
     }
-  }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (sync) {
+                while (sync.tura != 1) {
+                    try {
+                        sync.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(1);
+                sync.tura = 2;
+                sync.notifyAll();
+            }
+        }
+    }
 }
 
 class T2 extends Thread {
-  private Sync sync;
 
-  public T2(Sync sync) {
-    this.sync = sync;
-  }
+    final private Sync sync;
 
-  @Override
-  public void run() {
-    for (int i = 0; i < 10; i++) {
-      synchronized (sync) {
-        while (sync.tura != 2) {
-          try {
-            sync.wait();
-          } catch (InterruptedException e) {
-            e.printStackTrace();
-          }
-        }
-        System.out.println(2);
-        sync.tura = 1;
-        sync.notifyAll();
-      }
+    public T2(Sync sync) {
+        this.sync = sync;
     }
-  }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (sync) {
+                while (sync.tura != 2) {
+                    try {
+                        sync.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(2);
+                sync.tura = 1;
+                sync.notifyAll();
+            }
+        }
+    }
 }
 
 public class Tury {
-  public static void main(String[] args) {
-    Sync sync = new Sync();
-    T1 t1 = new T1(sync);
-    T2 t2 = new T2(sync);
-    t1.start();
-    t2.start();
-  }
+
+    public static void main(String[] args) {
+        Sync sync = new Sync();
+        T1 t1 = new T1(sync);
+        T2 t2 = new T2(sync);
+        t1.start();
+        t2.start();
+    }
 }
